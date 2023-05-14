29bbd6552ae67f68519c5cd8a2edb38d2e28bf2d
Beau Sherman <brsherman88@gmail.com>
2023-05-13 19:56:20 -0700

    npm init and add custom hook code.

---
+++ use-typewriter.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+
+function useTypewriter({ text = 'Hello World!', direction = 'forward', repeat = 0, speed = 200 }) {
+  text = text ?? 'Hello World!';
+
+  const textArr = Array.isArray(text) ? text : [text];
+
+  const [message, setMessage] = useState('');
+  const [textCount, setTextCount] = useState(0);
+  const [repeatCount, setRepeatCount] = useState(repeat);
+
+  let interval;
+  let index = 0;
+
+  function type(str) {
+    interval = setInterval(() => {
+      if (index > str.length) {
+        clearInterval(interval);
+        if (direction === 'both') backspace(str);
+      } else {
+        setMessage(str.slice(0, index));
+        index++;
+      }
+    }, speed);
+  }
+
+  function backspace(str) {
+    interval = setInterval(() => {
+      if (index < 0) {
+        clearInterval(interval);
+        setTextCount(prevCount => ++prevCount);
+      } else {
+        setMessage(str.slice(0, index));
+        index--;
+      }
+    }, speed);
+  }
+
+  function typingInit() {
+    if (textCount >= textArr.length) {
+      if (repeatCount > 0) setRepeatCount(prev => prev - 1);
+      setTextCount(0);
+    } else {
+      if (direction === 'forward' || direction === 'both') {
+        type(textArr[textCount]);
+      } else if (direction === 'backward') {
+        backspace(textArr[textCount]);
+      }
+    }
+  }
+
+  useEffect(() => typingInit(), [textCount]);
+
+  return message;
+}
+
+export default useTypewriter;
